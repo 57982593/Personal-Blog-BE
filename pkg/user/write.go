97fc03000b45734f89b61394d895a56bfb227922
v_zuwwang <57982593@qq.com>
2020-11-24 19:55:56 +0800

    add test code

---
+++ pkg/user/write.go
@@ -0,0 +1,25 @@
+package user
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type User struct {
+	Name   string
+	Habits []string
+}
+
+func Write(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Custom-Header", "custom")
+	w.WriteHeader(201)
+
+	user := &User{
+		Name:   "wang",
+		Habits: []string{"balls", "running", "hiking"},
+	}
+
+	json, _ := json.Marshal(user)
+	w.Write(json)
+}
