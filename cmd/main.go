5b6b82a6117a2577375511acb29f2d48b1a641d0
wangzhen <57982593@qq.com>
2021-02-17 00:52:39 +0800

    add test code

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
-	"goTestProject/init"
+	database "goTestProject/init"
 	"goTestProject/pkg/test"
 	"goTestProject/pkg/user"
 	"io/ioutil"
@@ -11,10 +10,12 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	config := "D:/goTestProject/config/admin-local.yaml"
+	config := "../config/admin-local.yaml"
 	viper.SetConfigFile(config)
 	content, err := ioutil.ReadFile(config)
 	if err != nil {
@@ -26,12 +27,43 @@ func main() {
 	}
 	database.InitDatabase()
 	fmt.Println("Service started Successfully!")
-	http.HandleFunc("/menuList",user.GetMenuList)
-	http.HandleFunc("/getRoute",user.GetRoute)
+	http.HandleFunc("/menuList", user.GetMenuList)
+	http.HandleFunc("/getRoute", user.GetRoute)
 	http.HandleFunc("/createTable", test.CreateTable)
+	http.HandleFunc("/eachInsert",test.EachInsert)
 	http.HandleFunc("/selectAll", test.SelectAll)
+	//并发测试 需要将 go say("并发") 放在 say("测试") 前面，否则会变成顺序执行，具体原因未知
+	// go say("并发")
+	// say("测试")
+
+	//并发进程间通信 ch <- c 把c 发送至通道 
+	//c := <-ch 接收通道消息，并赋值给c 语言
+	s := []int{7, 2, 8, -9, 4, 0}
+	//声明通道
+    c := make(chan int)
+    go sum(s[:len(s)/2], c)//从开始到len(s)/2处的数组
+    go sum(s[len(s)/2:], c)//从len(s)/2 处到数组结尾
+    x, y := <-c, <-c // 从通道 c 中接收
+
+    fmt.Println(x, y, x+y)
+
 	err = http.ListenAndServe(":8088", nil)
 	if (err) != nil {
 		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+			fmt.Println("测试%d/n",v)
+			sum += v
+	}
+	c <- sum // 把 sum 发送到通道 c
+}
+
+func say(s string) {	for i := 0; i < 5; i++ {
+			time.Sleep(100 * time.Millisecond)
+			fmt.Println(s)
 	}
 }
