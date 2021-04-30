cfb73649df7f4ebeaf5ddbc965e689ae5645b08d
v_zuwwang <57982593@qq.com>
2021-04-30 14:15:00 +0800

    add chinese annotation

--- gin/main.go
+++ gin/main.go
@@ -9,6 +9,7 @@ func main() {
 	router := gin.Default()
 
 	// This handler will match /user/john but will not match /user/ or /user
+	// 这里可以使用 /user/自定义路由名称 来访问， c.Param 会接收到自定义得路由名称。
 	router.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.String(http.StatusOK, "Hello %s", name)
@@ -16,6 +17,7 @@ func main() {
 
 	// However, this one will match /user/john/ and also /user/john/send
 	// If no other routers match /user/john, it will redirect to /user/john/
+	//多重自定义路由，效果同上。
 	router.GET("/user/:name/*action", func(c *gin.Context) {
 		name := c.Param("name")
 		action := c.Param("action")
