1615c9ccd22d8d2ca992ada2765a3d69068b051c
v_zuwwang <57982593@qq.com>
2021-06-18 18:12:27 +0800

    add grpc-web

--- go.mod
+++ go.mod
@@ -3,13 +3,16 @@ module goTestProject
 go 1.15
 
 require (
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-playground/validator/v10 v10.5.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/improbable-eng/grpc-web v0.14.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/spf13/viper v1.7.1
 	github.com/ugorji/go v1.2.5 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
@@ -18,4 +21,5 @@ require (
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
 )
--- grpc/main.go
+++ grpc/main.go
@@ -1,27 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/spf13/viper"
-	database "goTestProject/init"
 	"goTestProject/grpc/controller"
+	database "goTestProject/init"
 	"goTestProject/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"io/ioutil"
 	"log"
-	"net"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":9060")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	config := "../goTestProject/config/admin-local.yaml"
+	//lis, err := net.Listen("tcp", ":9060")
+	//if err != nil {
+	//	log.Fatalf("failed to listen: %v", err)
+	//}
+	config := "./config/admin-local.yaml"
 	viper.SetConfigFile(config)
+
 	content, err := ioutil.ReadFile(config)
 	if err != nil {
 		panic(fmt.Sprintf("Read config file fail: %s", err.Error()))
@@ -31,12 +35,49 @@ func main() {
 		panic(fmt.Sprintf("Parse config file fail: %s", err.Error()))
 	}
 	database.InitDatabase()
+	port := viper.Get("port")
+	fmt.Println(port)
 	fmt.Println("Service started Successfully!")
 	s := grpc.NewServer()
 	proto.RegisterRootServer(s, &controller.Server{})
 	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	wrappedGrpc := grpcweb.WrapServer(s,
+		grpcweb.WithCorsForRegisteredEndpointsOnly(false),
+		grpcweb.WithOriginFunc(func(origin string) bool {
+			return true
+		}),
+	)
+	var serveAddr string
+	flag.StringVar(&serveAddr,
+		"address",
+		fmt.Sprintf(":%d", port),
+		fmt.Sprintf(":serve address - e.g. :%d", port))
+	flag.Parse()
+
+	grpcweb.WithCorsForRegisteredEndpointsOnly(true)
+
+	srv := &http.Server{
+		Addr:         serveAddr,
+		WriteTimeout: time.Second * 32,
+		ReadTimeout:  time.Second * 32,
+		IdleTimeout:  time.Second * 60,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if wrappedGrpc.IsGrpcWebRequest(r) {
+				// NOT WORKING!
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				wrappedGrpc.ServeHTTP(w, r)
+				return
+			}
+
+			// Fall back to other servers.
+			http.DefaultServeMux.ServeHTTP(w, r)
+		}),
 	}
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(fmt.Sprintf("srv.ListenAndServe failed to serve: %v", err))
+	}
+	//if err := s.Serve(lis); err != nil {
+	//	log.Fatalf("s.Serve failed to serve: %v", err)
+	//}
 }
 
